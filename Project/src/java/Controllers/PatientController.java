258cfe51aa9c2e74cc5166f1e8435dadd5298121
William Lindsay <william.fr.lindsay@gmail.com>
2014-03-27 14:35:01 -0400

    fix for search as doctor

--- Project/src/java/Controllers/PatientController.java
+++ Project/src/java/Controllers/PatientController.java
@@ -110,7 +110,7 @@ public class PatientController {
         ArrayList<Patient> ret;
 
         try {
-            String stmt = "SELECT * FROM (SELECT DISTINCT * FROM ((SELECT User.user_id,User.first_name,User.last_name,User.email,Patient.address,Patient.current_health,Patient.ohip,Patient.phone,Patient.sin FROM User,Patient WHERE User.user_id = Patient.user_id) AS q1 LEFT OUTER JOIN (SELECT Doctor_Patient.patient_user_id,Doctor_Staff.permission FROM Doctor_Staff, Doctor_Patient WHERE Doctor_Staff.staff_user_id = ? AND Doctor_Patient.doctor_user_id = Doctor_Staff.doctor_user_id AND Doctor_Patient.default_doctor = 1) AS q2 on q1.user_id = q2.patient_user_id)) AS q3 LEFT OUTER JOIN (SELECT * FROM Doctor_Patient WHERE Doctor_Patient.default_doctor = 1) AS q4 on q3.user_id = q4.patient_user_id"
+            String stmt = "SELECT * FROM ((SELECT DISTINCT * FROM ((SELECT User.user_id,User.first_name,User.last_name,User.email,Patient.address,Patient.current_health,Patient.ohip,Patient.phone,Patient.sin FROM User,Patient WHERE User.user_id = Patient.user_id) AS q1 LEFT OUTER JOIN (SELECT Doctor_Patient.patient_user_id,Doctor_Staff.permission FROM Doctor_Staff, Doctor_Patient WHERE Doctor_Staff.staff_user_id = ? AND Doctor_Patient.doctor_user_id = Doctor_Staff.doctor_user_id AND Doctor_Patient.default_doctor = 1) AS q2 on q1.user_id = q2.patient_user_id)) AS q3 LEFT OUTER JOIN (SELECT * FROM Doctor_Patient WHERE Doctor_Patient.default_doctor = 1) AS q4 on q3.user_id = q4.patient_user_id)"
                         + "WHERE user_id LIKE ? OR first_name LIKE ? OR last_name LIKE ? OR email LIKE ? OR address LIKE ? OR current_health LIKE ? OR ohip LIKE ? OR phone LIKE ? OR sin LIKE ?";
             
             pstmt = con.prepareStatement(stmt);
@@ -145,12 +145,11 @@ public class PatientController {
         ArrayList<Patient> ret;
 
         try {
-            String stmt = "SELECT * FROM User, Patient, Doctor_Patient "
-                    + "WHERE User.user_id = Patient.user_id AND Doctor_Patient.patient_user_id = User.user_id "
-                    + "AND (User.user_id LIKE ? OR User.first_name LIKE ? OR User.last_name LIKE ? OR User.email LIKE ? OR Patient.address LIKE ? OR Patient.current_health LIKE ? OR Patient.ohip LIKE ? OR Patient.phone LIKE ? OR Patient.sin LIKE ?) AND Doctor_Patient.doctor_user_id = ?";
+            String stmt = "SELECT * FROM (SELECT DISTINCT User.user_id,User.first_name,User.last_name,User.email,Patient.address,Patient.current_health,Patient.ohip,Patient.phone,Patient.sin FROM User,Doctor_Patient,Patient WHERE User.user_id = Doctor_Patient.patient_user_id AND User.user_id = Patient.user_id AND Doctor_Patient.doctor_user_id = ?) AS q3 LEFT OUTER JOIN (SELECT * FROM Doctor_Patient WHERE Doctor_Patient.default_doctor = 1) AS q4 on q3.user_id = q4.patient_user_id"
+                    + " WHERE (user_id LIKE ? OR first_name LIKE ? OR last_name LIKE ? OR email LIKE ? OR address LIKE ? OR current_health LIKE ? OR ohip LIKE ? OR phone LIKE ? OR sin LIKE ?)";
             
             pstmt = con.prepareStatement(stmt);
-            pstmt.setString(1, "%"+query+"%");
+            pstmt.setString(1, doctor_id);
             pstmt.setString(2, "%"+query+"%");
             pstmt.setString(3, "%"+query+"%");
             pstmt.setString(4, "%"+query+"%");
@@ -159,14 +158,14 @@ public class PatientController {
             pstmt.setString(7, "%"+query+"%");
             pstmt.setString(8, "%"+query+"%");
             pstmt.setString(9, "%"+query+"%");
-            pstmt.setString(10, doctor_id);
+            pstmt.setString(10, "%"+query+"%");
             
             ResultSet resultSet;
             resultSet = pstmt.executeQuery();
  
             ret = new ArrayList<Patient>();
             while (resultSet.next()) {
-                ret.add(new Patient(resultSet));
+                ret.add(new Patient(resultSet, false));
             }
             return ret;
         } finally {
